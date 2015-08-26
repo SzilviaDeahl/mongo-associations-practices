4e5d00f482bdf3d8099ac2b63bbd3b66be3af252
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-25 21:04:19 -0600

    more routes added for initial setup

---
+++ practice1/routes/meetups.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var db = require('monk')('localhost/mongo-associations-lesson');
+var meetupsCollection = db.get('meetups')
+var locationsCollection = db.get('locations')
+var usersCollection = db.get('users')
