4c0838dc3a9aa9e9e862a5a378357b80820d096c
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-25 21:01:59 -0600

    routes to index page added

--- practice1/routes/index.js
+++ practice1/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/mongo-associations-lesson')
+var meetups = db.get('meetups')
+var locations = db.get('locations')
+var users = db.get('users')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  meetups.find({}, function (err, documents) {
+    if(err) throw err
+    res.render('index', { meetups: documents });
+  })
 });
 
 module.exports = router;
