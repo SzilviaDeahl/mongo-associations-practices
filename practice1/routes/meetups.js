83023503ba1917518c38ce4bcf98ad09c6b5fd39
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-26 11:34:31 -0600

    route to show page set up

--- practice1/routes/meetups.js
+++ practice1/routes/meetups.js
@@ -5,6 +5,10 @@ var meetupsCollection = db.get('meetups')
 var locationsCollection = db.get('locations')
 var usersCollection = db.get('users')
 
-
+router.get('/meetups/:id', function (req, res, next) {
+  meetupsCollection.findOne({_id: req.params.id}, function (err, meetup) {
+    res.render('meetups/show', {meetup: meetup})
+  });
+});
 
 module.exports = router;
