130b33ec1e9760500558f8d4e365d3e0a15f9d32
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-26 14:30:55 -0600

    assignment finished with all the routes added

--- practice1/routes/meetups.js
+++ practice1/routes/meetups.js
@@ -6,8 +6,35 @@ var locationsCollection = db.get('locations')
 var usersCollection = db.get('users')
 
 router.get('/meetups/:id', function (req, res, next) {
+  var meetupIds = [];
   meetupsCollection.findOne({_id: req.params.id}, function (err, meetup) {
-    res.render('meetups/show', {meetup: meetup})
+    locationsCollection.findOne({_id: meetup.locationId}, function (err, location) {
+      usersCollection.find({_id: {$in: meetup.memberIds}}, function (err, members) {
+        usersCollection.find({meetupIds: {$in: [meetup.meetupId]}}, function (err, followers) {
+
+          collectIds = function (user) {
+            for(var i =0; i < user.follows.length; i ++ ) {
+              if (req.params.id !== user.follows[i].toString()) {
+                meetupIds.push(user.follows[i])
+              }
+            }
+          }
+
+          followers.forEach(collectIds);
+          members.forEach(collectIds);
+
+          meetupsCollection.find({_id: {$in: meetupIds}}, function (err, other ) {
+            res.render('meetups/show', {
+            meetup: meetup,
+            location: location,
+            user: members,
+            followers: followers,
+            otherMeetups: other
+            });
+          });
+        });
+      });
+    });
   });
 });
 
