adbb53307d857171e6cc025af85eef84cb19b2df
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-28 15:34:02 -0600

    routes set up to access data

---
+++ practice2/routes/meetups.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var db = require('monk')('localhost/mongo-associations-lesson')
+var meetupsCollection = db.get('meetups');
+var locationsCollection = db.get('locations');
+var usersCollection = db.get('users');
+
+res.render('/meetups/:id', function (req, res, next) {
+  meetupsCollection.findOne({_id: req.params.id}, function (err, meetup) {
+    locationsCollection.findOne({_id: meetup.locationId}, function (err, location) {
+      usersCollection.find({_id: {$in:meetup.memberIds}}, function (err, members) {
+
+        res.render('meetups', {meetup: meetup, location: location, user: members})
+      });
+    });
+  });
+});
+
+module.exports = router;
