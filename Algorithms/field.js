8c34fe3f165ad0e1eee602498563bc39e4e03299
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-31 15:59:06 -0600

    algorithm finished

--- Algorithms/field.js
+++ Algorithms/field.js
@@ -10,19 +10,32 @@ function findDistance(node1, node2) {
   return Math.hypot(node2.x - node1.x, node2.y - node1.y)
 }
 
-var player1 = new Coordinates(2, 1, 'player2');
-var player2 = new Coordinates(4, 3, 'player1');
-var player3 = new Coordinates(6, 5, 'player4');
-var player4 = new Coordinates(8, 7, 'player3');
-var player5 = new Coordinates(10, 9, 'player5');
+var player1 = new Coordinates(22, 11, 'player1');
+var player0 = new Coordinates(44, 3, 'player0');
+var player4 = new Coordinates(6, 5, 'player4');
+var player2 = new Coordinates(8, 77, 'player2');
+var player3 = new Coordinates(10, 9, 'player3');
 
 var flag = new Coordinates(0, 0, 'flag');
 
 var something = [];
-something.push(findDistance(player1, flag));
-something.push(findDistance(player2, flag));
-something.push(findDistance(player3, flag));
-something.push(findDistance(player4, flag));
-something.push(findDistance(player5, flag));
+something.push(player0);
+something.push(player1);
+something.push(player2);
+something.push(player3);
+something.push(player4);
 
-console.log(something);
+var closest_player = player0;
+var closest_player_distance = findDistance(closest_player, flag);
+
+for (var i = 1; i < something.length; i++) {
+  new_dist = findDistance(something[i],flag);
+  if (new_dist < closest_player_distance ) {
+    closest_player  = something[i];
+    closest_player_distance = new_dist;
+  }
+}
+
+// Math.min.apply(Math, [player0.distance,player1.distance,]);
+
+console.log("closest player is "+ closest_player.type);
