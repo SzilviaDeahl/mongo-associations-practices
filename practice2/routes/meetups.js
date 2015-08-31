c07027442175e549ef0461a1e8f1dd4727b37aea
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-30 20:31:48 -0600

    routes fixed

--- practice2/routes/meetups.js
+++ practice2/routes/meetups.js
@@ -9,7 +9,6 @@ res.render('/meetups/:id', function (req, res, next) {
   meetupsCollection.findOne({_id: req.params.id}, function (err, meetup) {
     locationsCollection.findOne({_id: meetup.locationId}, function (err, location) {
       usersCollection.find({_id: {$in:meetup.memberIds}}, function (err, members) {
-//this is some code
         res.render('meetups', {meetup: meetup, location: location, user: members})
       });
     });
